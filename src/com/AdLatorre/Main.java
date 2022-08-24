f7435e729b602ecefce79884a661ad019c7585c3
shinobirun <adrian.latorre@gmail.com>
2022-08-24 14:01:36 -0300

    final

---
+++ src/com/AdLatorre/Main.java
@@ -0,0 +1,51 @@
+package com.AdLatorre;
+
+public class Main {
+    public static void main(String[] args) {
+
+         byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4L;
+
+        //punto flotante
+
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        // caracter
+
+        char caracter1='a';
+
+        //booleanos
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //cadenas
+
+        String nombre = "Adri";
+        String apellido = "Lato";
+
+        // tipos envoltorio
+
+        Integer numero = null;
+        Long numero2= 2L;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+        System.out.println(caracter1);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(numero);
+        System.out.println(numero2);
+
+
+    }
+}
